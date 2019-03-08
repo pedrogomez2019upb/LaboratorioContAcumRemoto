819f4e943e49030ffd1ea22b2d36fe549be442f8
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-08 11:09:43 -0500

    Ya casi calculo un primo.

--- primos.py
+++ primos.py
@@ -1,17 +1,11 @@
 #Algoritmo que lea un número entero a , lea los numeros y imprima el residuo de su división entre ellos 
-#primero nos toca hacer un contador igual a 0 para el for y tambien para la division
-i= 0
-division = 0
+#primero nos toca hacer un contador igual a 0 para el for
+y= 0
 #Recibimos los números a analizar
-a=int(input("Bienvenido ! Vamos a revisar la división de a números entre ellos . ¿Cuántos números quieres analizar?: "))
+a=int(input("Bienvenido ! Vamos a revisar la división de a números entre ellos . ¿Cuan número quieres analizar?: "))
 #Ponemos la condicional for para empezar el ejercicio
-for i in range (1,a+1):
-    x=int(input("Ingresa el valor: "))
-    division += a
-    division_resultado = division/x
-
-
-print("La división entre estos números son:{}. Gracias por utilizar el programa.".format(division_resultado)
-
-
+for i in range (1,a+1,1):
+    if a % i  ==0:
+        y = y +1
+print("La división entre estos números son: {}. Gracias por utilizar el programa.".format(y))
 #Desarrollado por Pedro Gómez / ID:000396221
