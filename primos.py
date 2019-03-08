87f432d4bec3f6cfb5fe52e390c0f95ef3bd1e6f
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-08 10:56:10 -0500

    Se hace una prueba ambulatoria para ver como funciona el programa.

--- primos.py
+++ primos.py
@@ -1,10 +1,17 @@
-#Crear un algorito que permita revisar si a es divisible entre b
-#Primero damos un mensaje de bienvenida y recibimos el primer valor y segundo
-x=int(input("Bienvenido! Vamos a ver si un número es exactamente divisible entre otro. Por favor ingresa el primer valor: "))
-y=int(input("Por favor ingresa el segundo valor: "))
-#Ponemos un if para poner la condicional
-if x%y==0 :
-    print("El número se divide exactamente.")
-else:
-    print("El número no se divide exactamente.")
+#Algoritmo que lea un número entero a , lea los numeros y imprima el residuo de su división entre ellos 
+#primero nos toca hacer un contador igual a 0 para el for y tambien para la division
+i= 0
+division = 0
+#Recibimos los números a analizar
+a=int(input("Bienvenido ! Vamos a revisar la división de a números entre ellos . ¿Cuántos números quieres analizar?: "))
+#Ponemos la condicional for para empezar el ejercicio
+for i in range (1,a+1):
+    x=int(input("Ingresa el valor: "))
+    division += a
+    division_resultado = division/x
+
+
+print("La división entre estos números son:{}. Gracias por utilizar el programa.".format(division_resultado)
+
+
 #Desarrollado por Pedro Gómez / ID:000396221
