a731d55473003f6433f91a87303487c87c9a3447
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-08 11:14:33 -0500

    Ejercicio Resuelto.

--- primos.py
+++ primos.py
@@ -2,10 +2,14 @@
 #primero nos toca hacer un contador igual a 0 para el for
 y= 0
 #Recibimos los números a analizar
-a=int(input("Bienvenido ! Vamos a revisar la división de a números entre ellos . ¿Cuan número quieres analizar?: "))
+a=int(input("Bienvenido !  Ingresa el número: "))
 #Ponemos la condicional for para empezar el ejercicio
 for i in range (1,a+1,1):
     if a % i  ==0:
         y = y +1
-print("La división entre estos números son: {}. Gracias por utilizar el programa.".format(y))
+#Para saber si es primo utilzamos un if then después del for
+if y > 2:
+        print("El número {} no es primo . Gracias por utilizar el programa.".format(y))
+else:
+        print("El número {} es primo. Gracas por utilizar el programa.".format(y))
 #Desarrollado por Pedro Gómez / ID:000396221
